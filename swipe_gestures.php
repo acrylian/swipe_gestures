bcb8cc798638efdf0214909b49f5efb9de059e81
acrylian <wartgart@gmx.net>
2013-06-09 13:22:08 +0200

    Since not an official plugin use proper version number

--- swipe_gestures.php
+++ swipe_gestures.php
@@ -11,6 +11,7 @@
 $plugin_is_filter = 9|THEME_PLUGIN;
 $plugin_description = gettext('A simple plugin that enabled touch screen left and right swiping on the single image page. Based on the jQuery plugin touchSwipe.');
 $plugin_author = 'Malte Müller (acrylian)';
+$plugin_version = '1.0';
 $option_interface = 'swipeGestures';
 zp_register_filter('theme_head','swipeGestures::swipejs');
 
