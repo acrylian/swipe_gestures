ebbfe2b56166fa28ef988350b6e4505c4741f14d
Malte Müller <acrylian@users.noreply.github.com>
2015-01-31 11:25:21 +0100

    Version number update

--- swipe_gestures.php
+++ swipe_gestures.php
@@ -4,7 +4,7 @@
  * Based on the jQuery plugin touchSwipe http://labs.rampinteractive.co.uk/touchSwipe
  *
  * @author Malte Müller (acrylian) <info@maltem.de>
- * @copyright 2014 Malte Müller
+ * @copyright 2015 Malte Müller
  * @license GPL v3 or later
  * @package plugins
  * @subpackage misc
@@ -13,7 +13,7 @@
 $plugin_is_filter = 9|THEME_PLUGIN;
 $plugin_description = gettext('A simple plugin that enabled touch screen left and right swiping on the single image page. Based on the jQuery plugin touchSwipe.');
 $plugin_author = 'Malte Müller (acrylian)';
-$plugin_version = '1.0.1';
+$plugin_version = '1.0.2';
 $option_interface = 'swipeGestures';
 zp_register_filter('theme_head','swipeGestures::swipejs');
 
